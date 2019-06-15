d8b1764fb323af1f5b8ac55f803641848a142e40
nawatend <32384185+nawatend@users.noreply.github.com>
2019-06-15 22:40:30 +0200

    free pw

--- parkspot_backoffice/src/server/api/v1/database/schemas/user.schema.js
+++ parkspot_backoffice/src/server/api/v1/database/schemas/user.schema.js
@@ -107,10 +107,12 @@ UserSchema.pre('save', function (next) {
 
 UserSchema.methods.comparePassword = function (candidatePassword, cb) {
     const user = this;
-    bcrypt.compare(candidatePassword, user.localProvider.password, (err, isMatch) => {
+    bcrypt.compare(candidatePassword, user.localProvider, (err, isMatch) => {
         if (err) {
+            console.log(err);
             return cb(err, null);
         }
+        console.log(isMatch);
         return cb(null, isMatch);
     });
 };
